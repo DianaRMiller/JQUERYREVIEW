2445360ad6bfdc03947e8718715c8b06c5c69783
diana <diana.r.miller@gmail.com>
2015-10-13 22:38:28 -0700

    add more favorite things to list

--- js/scripts.js
+++ js/scripts.js
@@ -2,16 +2,27 @@ $(document).ready(function() {
   $("#blanks form").submit(function(event) {
     var nameInput = $("input#name").val();
     var lastNameInput = $("input#lastName").val();
+    var nickNameInput = $("input#nickName").val();
     var favoriteFoodInput= $("input#favoriteFood").val();
     var favoriteColorInput= $("input#favoriteColor").val();
     var favoriteHobbieInput= $("input#favoriteHobbie").val();
+    var favoritePetInput= $("input#favoritePet").val();
+    var favoriteCityInput= $("input#favoriteCity").val();
+    var favoriteDrinkInput= $("input#favoriteDrink").val();
+    var favoriteSingerInput= $("input#favoriteSinger").val();
 
     $(".name").text(nameInput);
     $(".lastName").text(lastNameInput);
-    $(".age").text(ageInput);
+    $(".nickName").text(nickNameInput);
     $(".favoriteFood").text(favoriteFoodInput);
     $(".favoriteColor").text(favoriteColorInput);
     $(".favoriteHobbie").text(favoriteHobbieInput);
+    $(".favoritePet").text(favoritePetInput);
+    $(".favoriteCity").text(favoriteCityInput);
+    $(".favoriteDrink").text(favoriteDrinkInput);
+    $(".favoriteSinger").text(favoriteSingerInput);
+
+    $("#story").show();
 
     event.preventDefault();
   });
